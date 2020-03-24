f61a90503b9a606a1c09a9f6c532ad147b3f5a64
Santiagobocel <santiagobocel10@gmail.com>
2020-03-23 22:05:06 -0600

    controlador codificar

--- Codificacion/Controllers/CodificacionController.cs
+++ Codificacion/Controllers/CodificacionController.cs
@@ -8,6 +8,34 @@ namespace Codificacion.Controllers
 {
     public class CodificacionController : Controller
     {
-     
+        // cifrar
+        [HttpPost]
+        [Route("cipher/{nombre}")]
+        public ActionResult Insertar([FromBody] Datos_C Info, string nombre)
+        {
+            if (ModelState.IsValid)
+            {
+                switch ($"{nombre}")
+                {
+                    case "Cesar":
+                        Cesar.Cesar.Instance.Ingresar(Info.path);
+                        break;
+                    case "Zig-Zag":
+                        Zig_Zag.Zig_Zag.Instance.Ingresar(Info.path,Info.Carriles);
+                        break;
+                    case"Vertical" :
+                        vertical_espiral.vertical_espiral.Instance.Ingresar(Info.path, Info.filas);
+                        break;
+                    case "espiral":
+                        vertical_espiral.vertical_espiral.Instance.Ingresar(Info.path, Info.filas);
+                        break;
+                    default:
+                    //Error                     
+                        break;
+                }          
+                return Ok();
+            }
+            return BadRequest(ModelState);
+        }
     }
 }
