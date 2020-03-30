a3fcb94fe68ffebac13b72fe55fd273a8b21157d
PabloMuralles <pablomuralles28@gmail.com>
2020-03-29 18:43:58 -0600

    se creo el nuevo endpoint

--- Codificacion/Controllers/CodificacionController.cs
+++ Codificacion/Controllers/CodificacionController.cs
@@ -37,5 +37,38 @@ namespace Codificacion.Controllers
             }
             return BadRequest(ModelState);
         }
+        // decifrar
+        [HttpPost]
+        [Route("cipher/{nombre}")]
+        public ActionResult Decifrar([FromBody] Datos_C Info, string nombre)
+        {
+            if (ModelState.IsValid)
+            {
+                switch ($"{nombre}")
+                {
+                    case "Cesar":
+                        Cesar.Cesar.Instance.Ingresar(Info.path);
+                        break;
+                    case "Zig-Zag":
+                        Zig_Zag.Zig_Zag.Instance.Ingresar(Info.path, Info.Carriles);
+                        break;
+                    case "Vertical":
+                        vertical_espiral.vertical_espiral.Instance.Ingresar(Info.path, Info.filas);
+                        break;
+                    case "espiral":
+                        vertical_espiral.vertical_espiral.Instance.Ingresar(Info.path, Info.filas);
+                        break;
+                    default:
+                        //Error                     
+                        break;
+                }
+                return Ok();
+            }
+            return BadRequest(ModelState);
+        }
+
+
+
+
     }
 }
