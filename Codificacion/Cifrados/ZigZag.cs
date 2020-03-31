cd8f6ba9496bc16b9ecfa43b782f485e731e0d96
PabloMuralles <pablomuralles28@gmail.com>
2020-03-30 19:47:33 -0600

    se aplicaron cambios para que se pudiera tomar la nueva organiz

--- Codificacion/Cifrados/ZigZag.cs
+++ Codificacion/Cifrados/ZigZag.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Codificacion.Cifrados
 {
@@ -16,12 +17,130 @@ namespace Codificacion.Cifrados
                 return _instance;
             }
         }
-        public void Ingresar(string path, int carriles)
+       
+        public void Ingresar(string path, int niveles, string nombre)
         {
+            using (var archivo = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                using (var reader = new BinaryReader(archivo))
+                {
+                    var LongitudArchivo = Convert.ToInt32(reader.BaseStream.Length);
+                    byte[] buffer = new byte[LongitudArchivo];
+                    buffer = reader.ReadBytes(LongitudArchivo);
+                    var ArchivoCifrado = Cifrado(buffer, niveles);
+                    Escritura(ArchivoCifrado, nombre);
+
+
+                }
+
+            }
+        }
+
+        private byte[] Cifrado(byte[] Texto, int niveles_)
+        {
+            var TextoCifrado = new List<string>();
+            var Posicion = 0;
+            var Incrementador = 1;
+            var ElemetosXola = (2 * niveles_) - 2;
+            var CantidadOlas = (Texto.Length) / ElemetosXola;
+            CantidadOlas = CantidadOlas % 1 >= 0.5 ? Convert.ToInt32(CantidadOlas) : Convert.ToInt32(CantidadOlas) + 1;
+            var ElementosTotales = ElemetosXola * CantidadOlas;
+            var TextoCompleto = new byte[ElementosTotales];
+
+            for (int i = 0; i < Texto.Length; i++)
+            {
+                TextoCompleto[i] = Texto[i];
+            }
+
+            for (int i = Texto.Length; i < ElementosTotales; i++)
+            {
+
+                TextoCompleto[i] = Convert.ToByte(Convert.ToChar("$"));
+            }
+
+            for (int i = 0; i < niveles_; i++)
+            {
+                TextoCifrado.Add("");
+            }
+
+            foreach (var item in TextoCompleto)
+            {
+                if (Posicion + Incrementador == niveles_)
+                {
+                    Incrementador = -1;
+                }
+                else
+                {
+                    if (Posicion + Incrementador == -1)
+                    {
+                        Incrementador = 1;
+                    }
+                }
+
+                TextoCifrado[Posicion] += Convert.ToString(Convert.ToChar(item));
+                Posicion += Incrementador;
+            }
+
+            var CadenaCifrada = string.Empty;
+            foreach (var item in TextoCifrado)
+            {
+                CadenaCifrada += item;
+            }
+
+            var TextoCifradoBytes = new List<byte>();
+            foreach (var item in CadenaCifrada)
+            {
+                TextoCifradoBytes.Add(Convert.ToByte(Convert.ToChar(item)));
+            }
+
+            return TextoCifradoBytes.ToArray();
+        }
+
+
+        private void Escritura(byte[] TextoCifrado, string nombre_)
+        {
+            string CarpetaZigzagCifrado = Environment.CurrentDirectory;
+
+            if (!Directory.Exists(Path.Combine(CarpetaZigzagCifrado, "CifradoZigZag")))
+            {
+                Directory.CreateDirectory(Path.Combine(CarpetaZigzagCifrado, "CifradoZigZag"));
+            }
+
+            using (var streamwriter = new FileStream(Path.Combine(CarpetaZigzagCifrado, "CifradoZigZag", $"{nombre_}.text"), FileMode.OpenOrCreate))
+            {
+                using (var write = new BinaryWriter(streamwriter))
+                {
+                    write.Write(TextoCifrado);
+                }
+
+            }
 
         }
-        public void DecifrarIngresar( )
+
+
+        public void IngresarDecifrado(string path, int niveles, string nombre)
         {
+            using (var Archivo = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                using (var Reader = new BinaryReader(Archivo))
+                {
+                    var Longitud = Convert.ToInt32(Reader.BaseStream.Length);
+                    var buffer = new byte[Longitud];
+                    buffer = Reader.ReadBytes(Longitud);
+                    Decifrar(buffer, niveles);
+
+                }
+
+            }
+
+        }
+        private void Decifrar(byte[] TextoCifrado, int niveles)
+        {
+            var NivelesIntermedio = niveles - 2;
+
+            var CaracteresNivelesNoIntermedios = TextoCifrado.Length;
+
+
 
         }
     }
